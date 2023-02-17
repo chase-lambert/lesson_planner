32a11af473439bcce7e406bd5fa00bfb5ef4581a
Chase Lambert <chaselambert@gmail.com>
2023-02-16 19:36:46 -0500

    successfully sending form entry to OpenAI and receiving a response

--- src/query.rs
+++ src/query.rs
@@ -1,6 +1,6 @@
-// use reqwest::Client;
+use reqwest::Client;
 use serde::Deserialize;
-use serde_json::Value;
+// use serde_json::Value;
 
 #[derive(Debug, Deserialize)]
 pub struct Post {
@@ -23,32 +23,33 @@ pub struct Choice {
 #[allow(dead_code)]
 pub async fn run_query(prompt: &str) -> Result<Post, reqwest::Error> {
     println!("Your prompt: {prompt}");
-    // let api_key = std::env::var("OPEN_AI").expect("No variable found");
-    // let client = Client::new();
-    // let response_body: Post = client
-    //     .post("https://api.openai.com/v1/engines/davinci/completions")
-    //     .bearer_auth(api_key)
-    //     .json(&serde_json::json!({"prompt": prompt, "max_tokens": 50}))
-    //     .send()
-    //     .await?
-    //     .json()
-    //     .await?;
+    let api_key = std::env::var("OPEN_AI").expect("No variable found");
+    let client = Client::new();
+    let params = &serde_json::json!({"model": "text-ada-001", "prompt": prompt, "max_tokens": 100});
+    let response_body: Post = client
+        .post("https://api.openai.com/v1/completions")
+        .bearer_auth(api_key)
+        .json(params)
+        .send()
+        .await?
+        .json()
+        .await?;
 
-    let response_body = Post {
-        id: "cmpl-3OGvkhP19sWhlYZOT5UtAC7ajvDzy".to_owned(),
-        object: "text_completion".to_owned(),
-        created: 1626913964,
-        model: "davinci:2020-05-03".to_owned(),
-        choices: vec![Choice {
-            text: "... I had purple hair. It was a phase,\"I don't miss it because purpl
-    e has a lot of pigment. I mean it doesn't wash out. So again it is a throw away.\"\n\n2.
-    It motivated her to make better"
-                .to_owned(),
-            index: 0,
-            logprobs: Value::Null,
-            finish_reason: "length".to_owned(),
-        }],
-    };
+    // let response_body = Post {
+    //     id: "cmpl-3OGvkhP19sWhlYZOT5UtAC7ajvDzy".to_owned(),
+    //     object: "text_completion".to_owned(),
+    //     created: 1626913964,
+    //     model: "davinci:2020-05-03".to_owned(),
+    //     choices: vec![Choice {
+    //         text: "... I had purple hair. It was a phase,\"I don't miss it because purpl
+    // e has a lot of pigment. I mean it doesn't wash out. So again it is a throw away.\"\n\n2.
+    // It motivated her to make better"
+    //             .to_owned(),
+    //         index: 0,
+    //         logprobs: Value::Null,
+    //         finish_reason: "length".to_owned(),
+    //     }],
+    // };
     // println!("{:#?}", response_body);
     // println!("");
     // println!("Once upon a time {}", response_body.choices[0].text);
