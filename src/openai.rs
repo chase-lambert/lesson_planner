5f6213f0075b3850db3cff30f6003601ae3a5cb6
Chase Lambert <chaselambert@gmail.com>
2023-06-05 23:24:24 -0500

    further tweaks on lesson builder, added tailwind/typography plugin

--- src/openai.rs
+++ src/openai.rs
@@ -22,12 +22,34 @@ pub struct Choice {
 
 // #[allow(dead_code)]
 pub async fn run_query(prompt: &str) -> Result<Post, reqwest::Error> {
-    let initial_prompt = "You are a great educational assistant who helps teachers build thorough lesson plans. Do not include a needed materials section. For each step of the lesson plan, please provide the actual solution. So if you suggest having the students fill out a fill in the blank worksheet, please provide the actual worksheet with example problems. Also provide a teacher's version of said worksheet with the answers. Clearly divide each section with two carriage breaks. ";
-    let full_prompt = format!("{initial_prompt}. The lesson prompt for you to create is: {prompt}");
+    let subject = "English";
+    let grade_level = "10th Grade";
+    let prompt = format!(
+        r#"""
+As a {subject} teacher, I need a comprehensive lesson plan on {prompt} for my {grade_level} students. The lesson plan should cover the following aspects:
+
+1. Objective: Engage students in understanding the key concepts, skills, or themes related to {prompt}.
+2. Warm-up Activity: Begin the lesson with an engaging activity or discussion to pique students' interest and activate prior knowledge.
+3. Introduction: Provide background information or context on {prompt} to set the foundation for the lesson.
+4. Content Exploration: Explore the main content or skills related to {prompt} through interactive teaching methods such as presentations, demonstrations, or multimedia resources.
+5. Hands-on Practice: Engage students in hands-on activities, exercises, or experiments that reinforce their understanding of {prompt}.
+6. Application and Extension: Provide opportunities for students to apply what they have learned to real-life situations or engage in higher-order thinking tasks to extend their understanding.
+7. Assessment: Include formative or summative assessment strategies to gauge students' learning and provide feedback.
+8. Differentiation: Consider strategies to differentiate instruction based on students' varying abilities, interests, or learning styles.
+9. Resources and Materials: Provide a list of additional resources, references, or materials that can further support students' learning on {prompt}.
+10. Reflection: Encourage students to reflect on their learning and make connections to their own lives or the world around them.
+
+Please generate a detailed lesson plan with step-by-step instructions, engaging activities, and suitable assessment methods for {grade_level} students. The lesson plan should be aligned with the {subject} curriculum standards and incorporate interactive and student-centered teaching strategies. Please make sure the response is Markdown formatted just like this prompt is.
+
+Thank you!
+"""#
+    );
 
     let api_key = std::env::var("OPENAI_API_KEY").expect("No variable found");
     let client = Client::new();
-    let params = &serde_json::json!({"model": "text-davinci-003", "prompt": full_prompt, "max_tokens": 1000});
+    let model = "text-davinci-003";
+    // let model = "gpt-4";
+    let params = &serde_json::json!({"model": model, "prompt": prompt, "max_tokens": 1000});
     let response_body: Post = client
         .post("https://api.openai.com/v1/completions")
         .bearer_auth(api_key)
@@ -56,5 +78,6 @@ pub async fn run_query(prompt: &str) -> Result<Post, reqwest::Error> {
     // println!("");
     // println!("Once upon a time {}", response_body.choices[0].text);
 
+    println!("{:#?}", response_body);
     Ok(response_body)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("daisyui")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
 };
